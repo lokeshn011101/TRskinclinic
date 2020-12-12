586ba3bfdfcfa4e0d70b06173f63745e83116fb0
Udaya N <udayaprakash2899@gmail.com>
2020-12-12 19:59:25 +0530

    :tada: Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="bg-gray-700 text-white text-4xl text-center">
+      hello world
+    </div>
+  );
+}
